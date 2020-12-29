c0b71ef45da0e7e0383b99f3e308200929b49335
zabhitak <54598293+zabhitak@users.noreply.github.com>
2020-12-29 21:33:43 +0530

    admin_dashboard added

--- public/assets1/js/animate.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  'use strict';
-
-  $("#animate-it").on('click', function(){
-    $("#img-animate").addClass($("#animation-selection").val());
-    $("#img-animate").on('animationend', function() { $("#img-animate").attr('class', 'animated'); })
-  });
-
-})(jQuery);
